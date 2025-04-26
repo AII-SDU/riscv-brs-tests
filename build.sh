db78e6dc4a9677eb740ee0dfe123ade5af83153a
DennisDing2003 <blacktang482@gmail.com>
2025-04-26 16:31:37 +0800

    fix: Delete set -e in build.sh
    Delete set -e in build.sh to avoid
    exiting unexpectedly.
    
    Signed-off-by:Zixuan Ding <dennisding2003@gmail.com>
    Co-authored-by: USER0FISH <libing1202@outlook.com>

--- build.sh
+++ build.sh
@@ -12,10 +12,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
-
-#!/bin/bash  
-set -e  
+# limitations under the License. 
 
 # Variables  
 SRC_DIR="$(pwd)/src"  
